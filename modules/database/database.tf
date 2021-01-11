ca2f59a1b9ab6a2473af7e8f1587657b20858300
Angel Nuñez <snahider@gmail.com>
2021-01-10 20:01:11 -0500

    create volume for database data

--- modules/database/database.tf
+++ modules/database/database.tf
@@ -8,6 +8,10 @@ terraform {
 
 variable mysql_dump{}
 
+resource "docker_volume" "mysql_data" {
+  name = "mysql_data"
+}
+
 resource "docker_image" "mysql" {
   name = "mysql:8"
   keep_locally = "true"
@@ -24,11 +28,15 @@ resource "docker_container" "mysql" {
     "MYSQL_ROOT_PASSWORD=${random_password.mysql_root_password.result}",
     "MYSQL_DATABASE=petclinic"
   ]
-  mounts {
-    source = "/var/lib/mysql"
-    target = "/var/lib/mysql"
-    type = "bind"
+  volumes {
+    volume_name = docker_volume.mysql_data.name
+    container_path = "/var/lib/mysql"
   }
+  # mounts {
+  #   source = "/var/lib/mysql"
+  #   target = "/var/lib/mysql"
+  #   type = "bind"
+  # }
   mounts {
     source = var.mysql_dump
     target = "/docker-entrypoint-initdb.d"
